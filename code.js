914d03c354c0a4d736babbb193b39f45c59dd31b
Andy Pymont <andypymont@gmail.com>
2017-12-06 15:55:20 +0000

    Refactor day 1 solution (DRY)

--- code.js
+++ code.js
@@ -2,23 +2,22 @@ function rolling_lookup(array, index) {
   while ( index >= array.length ) {
     index -= array.length
   }
-  let rv = array[index]
-  return rv
+  return array[index]
+}
+
+function calc_checksum(input, lookahead) {
+  digits = input.toString()
+                .split('')
+                .map((x) => parseInt(x, 10))
+  la = lookahead(digits)
+  return digits.map((x, index) => (x === rolling_lookup(digits, index+la)) ? x : 0)
+               .reduce((a, b) => a+b, 0)
 }
 
 function checksum(input) {
-  digits = input.toString()
-                .split('')
-                .map((x) => parseInt(x, 10))
-  return digits.map((x, index) => (x === rolling_lookup(digits, index+1)) ? x : 0)
-               .reduce((a, b) => a+b, 0)
+  return calc_checksum(input, (x) => 1)
 }
 
 function checksum2(input) {
-  digits = input.toString()
-                .split('')
-                .map((x) => parseInt(x, 10))
-  lookahead = (digits.length / 2)
-  return digits.map((x, index) => (x === rolling_lookup(digits, index+lookahead)) ? x : 0)
-               .reduce((a, b) => a+b, 0)
+  return calc_checksum(input, (x) => (x.length/2))
 }
