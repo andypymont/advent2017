a3d30691cbf54ca98ababba6872b5268e816833a
Andy Pymont <pymont.a@pg.com>
2017-12-07 15:47:52 +0000

    Limited progress on 07

--- 07/code.js
+++ 07/code.js
@@ -1,12 +1,13 @@
-function bottom_of_tower(text) {
-  const nodes = node_list_from_text(text)
-  const all_children = [].concat.apply([], nodes.map((node) => node['children']))
-  return nodes.map((node) => node['name'])
-              .filter((node) => all_children.indexOf(node) === -1)
-              [0]
+function bottom_of_tower(nodes) {
+  const all_children = [].concat.apply([], Object.keys(nodes).map((nodename) => nodes[nodename])
+                                                             .map((node) => node['children']))
+  return Object.keys(nodes).map((nodename) => nodes[nodename])
+                           .map((node) => node['name'])
+                           .filter((node) => all_children.indexOf(node) === -1)
+                           [0]
 }
 
-function node_list_from_text(text) {
+function nodes_from_text(text) {
   function node(nodetext) {
     let nodedata = nodetext.split(' -> ')
     return {
@@ -15,5 +16,41 @@ function node_list_from_text(text) {
       'children': nodedata[1] ? (nodedata[1].split(', ')) : []
     }
   }
-  return text.split('\n').map((nt) => node(nt))
+  let rv = {}
+  text.split('\n').map((nt) => node(nt))
+      .forEach((node) => rv[node['name']] = node)
+  return rv
+}
+
+function tower(text) {
+  const nodes = nodes_from_text(text)
+  const base_node = nodes[bottom_of_tower(nodes)]
+  function nodify_children(node) {
+    node['children'] = node['children'].map((nodename) => nodify_children(nodes[nodename]))
+    return node
+  }
+  return nodify_children(base_node)
+}
+
+function total_weight(node) {
+  return node['weight'] +
+         node['children'].map((child) => total_weight(child))
+                         .reduce((a, b) => a + b, 0)
+}
+
+function make_weight_correction(tower) {
+  
+}
+
+function correct_weight(tower) {
+  const child_weights = tower['children'].map((node) => total_weight(node))
+  function standard_weight(weight) {
+    return tower['children'].filter(child => (child.weight === weight)).length > 1
+  }
+  const standard_weight = child_weights.filter(standard_weight)
+                                       .reduce((a, b) => Math.max, 0)
+  const non_standard_weight = child_weights.filter((x) => !(x === standard_weight))
+                                           .reduce((a, b) => Math.max, 0)
+  const correction_needed = standard_weight - non_standard_weight
+  return make_weight_correction(tower, correction_needed)
 }
--- 07/tests.js
+++ 07/tests.js
@@ -30,64 +30,97 @@ leaf1 (61)
 leaf8 (57)`
 
 example3 =
-`leaf2 (61)
-root (41) -> branch1
-leaf3 (61)
-branch1 (68) -> leaf1, leaf2, leaf3
-leaf1 (61)`
+`leaf1 (10)
+leaf2 (5)
+leaf3 (10)
+leaf4 (30)
+branch1 (10) -> leaf1, leaf2, leaf3
+branch2 (10) -> leaf4
+root (10) -> branch1`
 
-QUnit.test('bottom_of_tower(example1) === "tknk"', function(assert) {
-  assert.equal(bottom_of_tower(example1), 'tknk')
+QUnit.test('nodes_from_text(example1)', function(assert) {
+  assert.deepEqual(nodes_from_text(example1),
+                   {'pbga': {'name': 'pbga', 'weight': 66, 'children': []},
+                    'xhth': {'name': 'xhth', 'weight': 57, 'children': []},
+                    'ebii': {'name': 'ebii', 'weight': 61, 'children': []},
+                    'havc': {'name': 'havc', 'weight': 66, 'children': []},
+                    'ktlj': {'name': 'ktlj', 'weight': 57, 'children': []},
+                    'fwft': {'name': 'fwft', 'weight': 72, 'children': ['ktlj', 'cntj', 'xhth']},
+                    'qoyq': {'name': 'qoyq', 'weight': 66, 'children': []},
+                    'padx': {'name': 'padx', 'weight': 45, 'children': ['pbga', 'havc', 'qoyq']},
+                    'tknk': {'name': 'tknk', 'weight': 41, 'children': ['ugml', 'padx', 'fwft']},
+                    'jptl': {'name': 'jptl', 'weight': 61, 'children': []},
+                    'ugml': {'name': 'ugml', 'weight': 68, 'children': ['gyxo', 'ebii', 'jptl']},
+                    'gyxo': {'name': 'gyxo', 'weight': 61, 'children': []},
+                    'cntj': {'name': 'cntj', 'weight': 57, 'children': []}
+                  })
 })
-QUnit.test('bottom_of_tower(example2) === "root"', function(assert) {
-  assert.equal(bottom_of_tower(example2), 'root')
+QUnit.test('nodes_from_text(example2)', function(assert) {
+  assert.deepEqual(nodes_from_text(example2),
+                   {'leaf4': {'name': 'leaf4', 'weight': 66, 'children': []},
+                    'leaf9': {'name': 'leaf9', 'weight': 57, 'children': []},
+                    'leaf2': {'name': 'leaf2', 'weight': 61, 'children': []},
+                    'leaf5': {'name': 'leaf5', 'weight': 66, 'children': []},
+                    'leaf7': {'name': 'leaf7', 'weight': 57, 'children': []},
+                    'branch3': {'name': 'branch3', 'weight': 72, 'children': ['leaf7', 'leaf8', 'leaf9']},
+                    'leaf6': {'name': 'leaf6', 'weight': 66, 'children': []},
+                    'branch2': {'name': 'branch2', 'weight': 45, 'children': ['leaf4', 'leaf5', 'leaf6']},
+                    'root': {'name': 'root', 'weight': 41, 'children': ['branch1', 'branch2', 'branch3']},
+                    'leaf3': {'name': 'leaf3', 'weight': 61, 'children': []},
+                    'branch1': {'name': 'branch1', 'weight': 68, 'children': ['leaf1', 'leaf2', 'leaf3']},
+                    'leaf1': {'name': 'leaf1', 'weight': 61, 'children': []},
+                    'leaf8': {'name': 'leaf8', 'weight': 57, 'children': []}
+                   })
+})
+QUnit.test('nodes_from_text(example3)', function(assert) {
+  assert.deepEqual(nodes_from_text(example3),
+                   {'leaf2': {'name': 'leaf2', 'weight': 5, 'children': []},
+                    'root': {'name': 'root', 'weight': 10, 'children': ['branch1']},
+                    'leaf3': {'name': 'leaf3', 'weight': 10, 'children': []},
+                    'branch1': {'name': 'branch1', 'weight': 10, 'children': ['leaf1', 'leaf2', 'leaf3']},
+                    'leaf1': {'name': 'leaf1', 'weight': 10, 'children': []},
+                    'leaf4': {'name': 'leaf4', 'weight': 30, 'children': []},
+                    'branch2': {'name': 'branch2', 'weight': 10, 'children': ['leaf4']}
+                   })
 })
 
-QUnit.test('node_list_from_text(example1)', function(assert) {
-  assert.deepEqual(node_list_from_text(example1),
-                   [{'name': 'pbga', 'weight': 66, 'children': []},
-                    {'name': 'xhth', 'weight': 57, 'children': []},
-                    {'name': 'ebii', 'weight': 61, 'children': []},
-                    {'name': 'havc', 'weight': 66, 'children': []},
-                    {'name': 'ktlj', 'weight': 57, 'children': []},
-                    {'name': 'fwft', 'weight': 72, 'children': ['ktlj', 'cntj', 'xhth']},
-                    {'name': 'qoyq', 'weight': 66, 'children': []},
-                    {'name': 'padx', 'weight': 45, 'children': ['pbga', 'havc', 'qoyq']},
-                    {'name': 'tknk', 'weight': 41, 'children': ['ugml', 'padx', 'fwft']},
-                    {'name': 'jptl', 'weight': 61, 'children': []},
-                    {'name': 'ugml', 'weight': 68, 'children': ['gyxo', 'ebii', 'jptl']},
-                    {'name': 'gyxo', 'weight': 61, 'children': []},
-                    {'name': 'cntj', 'weight': 57, 'children': []}
-                  ])
+QUnit.test('bottom_of_tower(nodes_from_text(example1)) === "tknk"', function(assert) {
+  assert.equal(bottom_of_tower(nodes_from_text(example1)), 'tknk')
 })
-QUnit.test('node_list_from_text(example2)', function(assert) {
-  assert.deepEqual(node_list_from_text(example2),
-                   [{'name': 'leaf4', 'weight': 66, 'children': []},
-                    {'name': 'leaf9', 'weight': 57, 'children': []},
-                    {'name': 'leaf2', 'weight': 61, 'children': []},
-                    {'name': 'leaf5', 'weight': 66, 'children': []},
-                    {'name': 'leaf7', 'weight': 57, 'children': []},
-                    {'name': 'branch3', 'weight': 72, 'children': ['leaf7', 'leaf8', 'leaf9']},
-                    {'name': 'leaf6', 'weight': 66, 'children': []},
-                    {'name': 'branch2', 'weight': 45, 'children': ['leaf4', 'leaf5', 'leaf6']},
-                    {'name': 'root', 'weight': 41, 'children': ['branch1', 'branch2', 'branch3']},
-                    {'name': 'leaf3', 'weight': 61, 'children': []},
-                    {'name': 'branch1', 'weight': 68, 'children': ['leaf1', 'leaf2', 'leaf3']},
-                    {'name': 'leaf1', 'weight': 61, 'children': []},
-                    {'name': 'leaf8', 'weight': 57, 'children': []}
-                  ])
-})
-QUnit.test('node_list_from_text(example3)', function(assert) {
-  assert.deepEqual(node_list_from_text(example3),
-                   [{'name': 'leaf2', 'weight': 61, 'children': []},
-                    {'name': 'root', 'weight': 41, 'children': ['branch1']},
-                    {'name': 'leaf3', 'weight': 61, 'children': []},
-                    {'name': 'branch1', 'weight': 68, 'children': ['leaf1', 'leaf2', 'leaf3']},
-                    {'name': 'leaf1', 'weight': 61, 'children': []}
-                  ])
+QUnit.test('bottom_of_tower(nodes_from_text(example2)) === "root"', function(assert) {
+  assert.equal(bottom_of_tower(nodes_from_text(example2)), 'root')
 })
 
 // Part 1 solution:
-QUnit.test('bottom_of_tower(puzzle_input)', function(assert) {
-  assert.equal(bottom_of_tower(puzzle_input), 'ykpsek')
+QUnit.test('bottom_of_tower(nodes_from_text(puzzle_input))', function(assert) {
+  assert.equal(bottom_of_tower(nodes_from_text(puzzle_input)), 'ykpsek')
+})
+
+// Part 2 tests:
+QUnit.test('tower(example1)', function(assert) {
+  let branch1 = {'name': 'ugml', 'weight': 68, 'children': [{'name': 'gyxo', 'weight': 61, 'children': []},
+                                                            {'name': 'ebii', 'weight': 61, 'children': []},
+                                                            {'name': 'jptl', 'weight': 61, 'children': []}]}
+  let branch2 = {'name': 'padx', 'weight': 45, 'children': [{'name': 'pbga', 'weight': 66, 'children': []},
+                                                            {'name': 'havc', 'weight': 66, 'children': []},
+                                                            {'name': 'qoyq', 'weight': 66, 'children': []}]}
+  let branch3 = {'name': 'fwft', 'weight': 72, 'children': [{'name': 'ktlj', 'weight': 57, 'children': []},
+                                                            {'name': 'cntj', 'weight': 57, 'children': []},
+                                                            {'name': 'xhth', 'weight': 57, 'children': []}]}
+  assert.deepEqual(tower(example1),
+                   {'name': 'tknk', 'weight': 41, 'children': [branch1, branch2, branch3]})
+})
+
+QUnit.test('total_weight() for ugml in example1', function(assert) {
+  assert.equal(total_weight(tower(example1)['children'][0]), 251)
+})
+QUnit.test('total_weight() for padx in example1', function(assert) {
+  assert.equal(total_weight(tower(example1)['children'][1]), 243)
+})
+
+QUnit.test('correct_weight(tower(example1)) returns 60', function(assert) {
+  assert.equal(correct_weight(tower(example1)), 60)
+})
+QUnit.test('correct_weight(tower(example3)) returns 10', function(assert) {
+  assert.equal(correct_weight(tower(example3)), 10)
 })
