48730572a2c0a7d3cb1ad2b3dff2ff38b09ccc6f
Andy Pymont <andypymont@gmail.com>
2017-12-11 05:49:00 +0000

    Day 11 Part 1

---
+++ 11/code.js
@@ -0,0 +1,42 @@
+function vector_to_coords(vector) {
+  return vector.split(',').map(x => parseInt(x, 10))
+}
+
+function add_vectors(v1, v2) {
+  const coords1 = vector_to_coords(v1)
+  const coords2 = vector_to_coords(v2)
+  return (coords1[0] + coords2[0]) + ',' + (coords1[1] + coords2[1])
+}
+
+const directions = {
+  'n': '0,1',
+  'ne': '1,0',
+  'se': '1,-1',
+  's': '0,-1',
+  'sw': '-1,0',
+  'nw': '-1,1'
+}
+
+function move_direction(position, direction) {
+  return add_vectors(position, directions[direction])
+}
+
+function distance(v1, v2) {
+  const coords1 = vector_to_coords(v1)
+  const coords2 = vector_to_coords(v2)
+
+  const distances_3d = [coords2[0] - coords1[0],
+                        coords2[1] - coords2[1],
+                        (0 - coords2[0] - coords2[1]) - (0 - coords1[0] - coords1[1])]
+                       .map(Math.abs)
+  return distances_3d.reduce((a, b) => Math.max(a, b), 0)
+}
+
+function distance_after_moves(movestext) {
+  const moves = movestext.split(',')
+  let location = '0,0'
+  moves.forEach(move => {
+    location = move_direction(location, move)
+  })
+  return distance('0,0', location)
+}
---
+++ 11/tests.js
@@ -0,0 +1,42 @@
+function test_vector_to_coords(vector, expected) {
+  QUnit.test('vector_to_coords("' + vector + '") === [' + expected.toString() + ']',
+             assert => assert.deepEqual(vector_to_coords(vector), expected))
+}
+test_vector_to_coords('0,0', [0, 0])
+test_vector_to_coords('20,13', [20, 13])
+test_vector_to_coords('-4,3', [-4, 3])
+
+function test_add_vectors(v1, v2, expected) {
+  QUnit.test('add_vector("' + v1 + '", "' + v2 + '") === "' + expected + '"',
+             assert => assert.equal(add_vectors(v1, v2), expected))
+}
+test_add_vectors('0,0', '1,4', '1,4')
+test_add_vectors('-3,-1', '-1,2', '-4,1')
+test_add_vectors('2,2', '-1,3', '1,5')
+
+function test_move_direction(coords, direction, expected) {
+  QUnit.test('move_direction("' + coords + '", "' + direction + '") === "' + expected + '"',
+             assert => assert.equal(move_direction(coords, direction), expected))
+}
+test_move_direction('0,0', 'ne', '1,0')
+test_move_direction('2,-1', 'n', '2,0')
+test_move_direction('0,-1', 'nw', '-1,0')
+
+function test_distance(v1, v2, expected) {
+  QUnit.test('distance("' + v1 + '", "' + v2 + '") === ' + expected,
+             assert => assert.equal(distance(v1, v2), expected))
+}
+test_distance('0,0', '3,0', 3)
+test_distance('4,4', '4,4', 0)
+test_distance('1,1', '3,-1', 2)
+test_distance('7,7', '7,4', 3)
+test_distance('0,0', '-3,-3', 6)
+
+function test_distance_after_moves(moves, expected) {
+  QUnit.test('distance_after_moves("' + moves + '") === ' + expected,
+             assert => assert.equal(distance_after_moves(moves), expected))
+}
+test_distance_after_moves('ne,ne,ne', 3)
+test_distance_after_moves('ne,ne,sw,sw', 0)
+test_distance_after_moves('ne,ne,s,s', 2)
+test_distance_after_moves('se,sw,se,sw,sw', 3)
