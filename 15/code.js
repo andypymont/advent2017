041a4a2953ea3f0d7cc13afda8a00c210424b7f0
Andy Pymont <andypymont@gmail.com>
2017-12-15 06:14:56 +0000

    Day 15 Part 2

--- 15/code.js
+++ 15/code.js
@@ -20,8 +20,32 @@ function judge(start, times) {
   const gen_b = advent_generator(start.B, 48271)
   let matches = 0
 
-  for ( let i = 0; i < times; i++ ) { 
+  for ( let i = 0; i < times; i++ ) {
     if ( binary_bottom16(gen_a.next().value) == binary_bottom16(gen_b.next().value) ) {
+      matches++
+    }
+  }
+  return matches
+}
+
+function *advent_generator2(start, multiply, checkfactor) {
+  const divide = 2147483647
+  let value = start
+  while ( true ) {
+    value = (value * multiply) % divide
+    if ( value % checkfactor === 0 ) {
+      yield value
+    }
+  }
+}
+
+function judge2(start, times) {
+  const gen_a = advent_generator2(start.A, 16807, 4)
+  const gen_b = advent_generator2(start.B, 48271, 8)
+  let matches = 0
+
+  for ( let i = 0; i < times; i++ ) {
+    if ( binary_bottom16(gen_a.next().value) === binary_bottom16(gen_b.next().value) ) {
       matches++
     }
   }
--- 15/tests.js
+++ 15/tests.js
@@ -34,3 +34,30 @@ QUnit.test('judge(t1, 5) === 1',
 // Part 1 solution
 // QUnit.test('judge(puzzle_input, 40000000) === 609',
 //            assert => assert.equal(judge(puzzle_input, 40000000), 609))
+
+// Part 2 tests:
+function test_advent_generator2(start, multiply, checkfactor, expected) {
+  QUnit.test('first ' + expected.length + ' values for advent_generator2(' + start + ', ' + multiply + ', ' + checkfactor + ') generated correctly',
+             function(assert) {
+               const gen = advent_generator2(start, multiply, checkfactor)
+               expected.forEach(item => assert.equal(gen.next().value, item))
+             })
+}
+test_advent_generator2(65, 16807, 4, [1352636452, 1992081072, 530830436, 1980017072, 740335192])
+test_advent_generator2(8921, 48271, 8, [1233683848, 862516352, 1159784568, 1616057672, 412269392])
+
+QUnit.test('advent_generator2(65, 16807, 4) and advent_generator2(8921, 48271, 8) first match binary_bottom16() of outputs on value 1056',
+           function(assert) {
+             const gen_a = advent_generator2(65, 16807, 4)
+             const gen_b = advent_generator2(8921, 48271, 8)
+             let check = 1
+             while ( true ) {
+               if ( binary_bottom16(gen_a.next().value) === binary_bottom16(gen_b.next().value) ) {
+                 break
+               }
+               check++
+             }
+             assert.equal(check, 1056)
+})
+QUnit.test('judge2(t1, 1056) === 1',
+           assert => assert.equal(judge2(t1, 1056), 1))
